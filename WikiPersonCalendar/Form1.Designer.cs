dc811b7d23943b9a80a8c9617c03fc189044ded6
Yuki-Kono213 <marushige9685@gmail.com>
2022-03-07 19:58:24 +0900

    プロジェクト ファイルを追加します。

---
+++ WikiPersonCalendar/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WikiPersonCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(393, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(13, 42);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 94);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(532, 318);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WikiPersonCalendar/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace WikiPersonCalendar
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "今日の日付";
+            // Longフォーマットのカレンダー
+            dateTimePicker2.Format = DateTimePickerFormat.Long;
+
+            DisplayDayPerson();
+
+
+        }
+
+        public void DisplayDayPerson()
+        {
+            string str = "https://ja.wikipedia.org/wiki/" + Convert.ToInt32(dateTimePicker2.Value.ToString("MM")).ToString()
+               + "%E6%9C%88" + Convert.ToInt32(dateTimePicker2.Value.ToString("dd")).ToString() + "%E6%97%A5";
+            label2.Text = str;
+
+            string Tani_str = "";
+            Uri webUri = new Uri(str);
+            List<Person> Personlist = new List<Person>();
+
+            WebClient client = new WebClient();
+            try
+            {
+                //docがHtmlのコードを文書として保持します
+                var doc = new HtmlAgilityPack.HtmlDocument();
+
+                //webを通してHtmlのコードを取得します
+                var web = new System.Net.WebClient();
+
+                //HtmlをUTF-8で取得します。
+                //特に指定しない場合、Htmlの文字コードによっては文字化けする場合があります
+                web.Encoding = Encoding.UTF8;
+
+                //web.DownloadString(URL)のURL先のHtmlを取得します。
+                //今回の場合はテキストボックスに書かれているURLを入れています
+                doc.LoadHtml(web.DownloadString(str));
+
+                //取得したHtmlコードからどの部分を取得するのかをXPath形式で指定します
+                //今回はブログの本文を指定しています
+                string Tani_XPath = "//*[self::h2 or self::li]";
+
+                //指定したXPathをもとに文を取得しています
+                var Tani_Collection = doc.DocumentNode.SelectNodes(Tani_XPath);
+                textBox1.Text = "";
+               bool BirthFalg = false;
+                //Tani_CollectionはListなので、一つずつ文を取り出しています
+                //文はInnerTextで取り出すことができます
+                Regex reg = new Regex("&..*;");
+                foreach (var tmp in Tani_Collection)
+                {
+                    if (tmp.InnerText.Contains("忌日[編集]"))
+                    {
+                        BirthFalg = false;
+                    }
+                    if (BirthFalg)
+                    {
+                        Person p = new Person();
+                        if (tmp.InnerText.Contains("B.C."))
+                        {
+                            p.BirthYear = -Int32.Parse(tmp.InnerText.Substring(0, tmp.InnerText.IndexOf("年")).Replace("B.C.",""));
+                        }
+                        else if (!tmp.InnerText.Contains("生年不明")) 
+                        {
+                            p.BirthYear = Int32.Parse(tmp.InnerText.Substring(0, tmp.InnerText.IndexOf("年")));
+                        }
+                        if ((dateTimePicker2.Value.Year - p.BirthYear) % 10 == 0)
+                        {
+                            if (tmp.InnerText.Contains("、")) {
+                                p.Name = tmp.InnerText.Substring(tmp.InnerText.IndexOf("-") + 1, tmp.InnerText.IndexOf("、") - tmp.InnerText.IndexOf("-") - 1);
+                                p.Name = reg.Replace(p.Name, "");
+                                Personlist.Add(p);
+                                Tani_str += p.Name + "　生誕" + (dateTimePicker2.Value.Year - p.BirthYear).ToString() + "年" + "\r\n";
+
+                            }
+                        }
+                    }
+                    if (tmp.InnerText.Contains("誕生日[編集]"))
+                    {
+                        BirthFalg = true;
+                    }
+
+
+                }
+
+                textBox1.Text += Tani_str;
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text += Tani_str;
+            }
+        }
+
+        private class Person
+        {
+            public int BirthYear;
+            public string Name;       
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            DisplayDayPerson();
+        }
+    }
+
+}
